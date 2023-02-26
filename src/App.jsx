de3224e11f829c4931cb150592c5e64ca28f2608
nioritos <junioradr2008@gmail.com>
2023-02-26 20:52:28 -0300

    doing the ui and going to get the weather informations

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import { useState } from 'react'
 import './css/App.css';
-
+import {AiOutlineSearch} from 'react-icons/ai';
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [local, setLocal] = useState('');
   return (
     <div className="App">
-     
+      <h1>Weathering</h1>
+      <h2>Only helping you with weather informations of your local.</h2>
+
+      <div className="weather">
+          <div className="weather-input">
+              <input type="text" name="local" id="local" placeholder='type your local name here' onChange={
+                 e => setLocal(e.target.value)
+              }/>
+            <button>
+              <AiOutlineSearch size={26}/>
+            </button>
+          </div>
+          <div className="weather-info">
+              
+          </div>
+      </div>
     </div>
   )
 }
