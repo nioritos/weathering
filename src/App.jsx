752fe365d1a73f9c7c3485ce08eb6edc39e9e88e
nioritos <junioradr2008@gmail.com>
2023-03-07 01:17:30 -0300

    background lefts

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
 import { useState } from 'react'
 import './css/App.css';
 import {AiOutlineSearch} from 'react-icons/ai';
-import { requestWeatherInfo } from './api/api';
+import {GoLocation} from 'react-icons/go';
+import {GiWaterDrop} from 'react-icons/gi';
+import {BsWind} from 'react-icons/bs'
+import { requestWeatherInfo, requestUnsplashImage, requestCountryFlag } from './api/api';
+const apiKey = import.meta.env.WEATHER_API_KEY;
 
 function App() {
   const [local, setLocal] = useState('');
-  const apiKey = 
-  
+  const [weather, setWeather] = useState('');
+  const [countryFlag, setCountryFlag] = useState('');
+  const [backgroundCountry, setBackgroundCountry] = useState('');
+
+  const onClickHandler = async () => {
+    const fields = await requestWeatherInfo('key=109547dac82b4fada42220908232602', local);
+    setWeather(fields.data);
+
+    searchFlag(fields.data)
+
+  }
+
+  const searchFlag = async (country) => {
+    const fieldsFlag = await requestCountryFlag(country.location.country);
+    setCountryFlag(fieldsFlag.config.url);
+  }
+
+  document.querySelector('.App').style.background()
+
   return (
     <div className="App">
       <h1>Weathering</h1>
@@ -17,16 +38,40 @@ function App() {
               <input type="text" name="local" id="local" placeholder='type your local name here' onChange={
                  e => setLocal(e.target.value)
               }/>
-            <button onClick={requestWeatherInfo}>
+            <button onClick={onClickHandler}>
               <AiOutlineSearch size={26}/>
             </button>
           </div>
           <div className="weather-info">
+              {
+                weather ? (
+                  <>
+                      <div className="header-weather-info">
+                      <GoLocation className='location-icon' size={24}/>
+                      <h2>{weather.location.name}</h2> 
+                      <img crossOrigin='anonymous' src={countryFlag} alt="" />
+                      </div>
+                      <h1>{weather.current.temp_c} °C</h1>
+                      <div className="conditions-weather-info">
+                        <h3>{weather.current.condition.text}</h3>
+                        <img src={weather.current.condition.icon} alt="" />
+                      </div>
 
+                      <div className="humidity-weather-info">
+                        <p>{weather.current.humidity}%</p>
+                        <GiWaterDrop size={24}/> |
+                        <BsWind size={24}/>
+                        <p>{weather.current.wind_kph}</p>
+                      </div>
+
+                  </>
+                 ) : console.log('loading...') 
+              }
           </div>
       </div>
     </div>
   )
+
 }
 
 export default App
--- src/api/api.js
+++ src/api/api.js
@@ -1,12 +1,32 @@
 import axios from 'axios'
 
-export const requestWeatherInfo = async (url, key, local) => {
+export const requestWeatherInfo = async ( key, local) => {
     try {
       let url = `https://api.weatherapi.com/v1/current.json?${key}&q=${local}&aqi=yes`
-      const response = axios.get(url);
-      console.log(response)
+      const response = await axios.get(url);
       return response
     } catch (error) {
-      
+        console.log(error)
     }
+  };
+
+export const requestUnsplashImage = async (location) => {
+  try {
+    let url = `https://source.unsplash.com/1600x900/?${location}`;
+    const response = await axios.get(url);
+    return response
+  } catch (err) {
+    console.log(err)
   }
+
+}
+export const requestCountryFlag = async (country) => {
+  try {
+    let url = `https://countryflagsapi.com/png/${country}`;
+    const response = await axios.get(url);
+    return response
+  } catch (error) {
+    console.log(error)
+  }
+}
+
